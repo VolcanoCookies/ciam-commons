4df799b27a2dcf0d04f3f693dce2072677a956ee
VolcanoCookies <franek.kg@gmail.com>
2022-04-01 12:11:41 +0200

    Created custom error for all Check methods

--- src/main.ts
+++ src/main.ts
@@ -5,32 +5,39 @@ export namespace Check {
     export const flagRegex: RegExp = /^(?:([a-z0-9]+|\?)(?:\.(?:[a-z0-9]+|\?))*(\.\*)?|\*)$/;
     export const strictFlagRegex: RegExp = /^[a-z0-9]+(\.[a-z0-9]+)*$/;
 
+    export class CheckError extends Error {
+        constructor(message: string) {
+            super(message);
+            this.name = 'CheckError';
+        }
+    }
+
     export function objectId(id: string, message: string = 'Invalid objectId') {
-        if (!id.match(objectIdRegex)) throw new Error(`${message} "${id}"`);
+        if (!id.match(objectIdRegex)) throw new CheckError(`${message} "${id}"`);
     }
 
     export function discordId(id: string, message: string = 'Invalid discordId') {
-        if (!id.match(discordIdRegex)) throw new Error(`${message} "${id}"`);
+        if (!id.match(discordIdRegex)) throw new CheckError(`${message} "${id}"`);
     }
 
     export function flag(flag: string, message: string = 'Invalid permission flag') {
-        if (!flag.match(flagRegex)) throw new Error(`${message} "${flag}"`);
+        if (!flag.match(flagRegex)) throw new CheckError(`${message} "${flag}"`);
     }
 
     export function strictFlag(flag: string, message: string = 'Invalid strict permission flag') {
-        if (!flag.match(strictFlagRegex)) throw new Error(`${message} "${flag}"`);
+        if (!flag.match(strictFlagRegex)) throw new CheckError(`${message} "${flag}"`);
     }
 
     export function notEmpty(obj: string | Array<any>, name: string) {
-        if (obj.length == 0) throw new Error(`${name} cannot be empty`);
+        if (obj.length == 0) throw new CheckError(`${name} cannot be empty`);
     }
 
     export function min(n: number, min: number, name: string) {
-        if (n < min) throw new Error(`${name} cannot be less than ${min}`);
+        if (n < min) throw new CheckError(`${name} cannot be less than ${min}`);
     }
 
     export function max(n: number, max: number, name: string) {
-        if (n > max) throw new Error(`${name} cannot be greater than ${max}`);
+        if (n > max) throw new CheckError(`${name} cannot be greater than ${max}`);
     }
 
     export function inRange(n: number, min: number, max: number, name: string) {
@@ -39,7 +46,7 @@ export namespace Check {
     }
 
     export function oneOf<T>(obj: T, options: Array<T>, name: string) {
-        if (!options.includes(obj)) throw new Error(`${name} must be one of ${options.toString()}`);
+        if (!options.includes(obj)) throw new CheckError(`${name} must be one of ${options.toString()}`);
     }
 
 }
